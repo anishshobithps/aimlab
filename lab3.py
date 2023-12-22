ab8a6e4a37511f472161add449872506634b4a66
anishshobithps <anish.shobith19@gmail.com>
2023-12-22 08:10:08 +0530

    shorter: lab 3
    Signed-off-by: anishshobithps <anish.shobith19@gmail.com>

--- lab3.py
+++ lab3.py
@@ -2,27 +2,19 @@ import pandas as pd
 
 df = pd.read_csv('datasets/3.csv')
 
-shypo = ['0'] * (length := df.iloc[:, :-1].shape[1])
-ghypo = [['?'] * length]
-
-def update_hypothesis(shypo, ghypo, row):
-    for i in range(len(shypo)):
-        if row.iloc[-1] == 1:
-            if shypo[i] != str(row.iloc[i]):
-                shypo[i] = '?' if shypo[i] != '0' else str(row.iloc[i])
-            ghypo_copy = ghypo.copy()
-            for g in ghypo_copy:
-                if g[i] != '?' and shypo[i] == '?':
-                    ghypo.remove(g)
-        elif row.iloc[-1] == 0:
-            for i in range(len(shypo)):
-                if str(row.iloc[i]) != shypo[i] and shypo[i] != '?':
-                    temp_list = ['?'] * i + [shypo[i]] + ['?'] * (len(shypo) - i - 1)
-                    if temp_list not in ghypo:
-                        ghypo.append(temp_list)
+shypo = ['0'] * len(df.columns[:-1])
+ghypo = [['?'] * len(shypo)]
 
 for _, row in df.iterrows():
-    update_hypothesis(shypo, ghypo, row)
+    for i, value in enumerate(row[:-1]):
+        if row.iloc[-1] == 1:
+            if shypo[i] != str(value):
+                shypo[i] = '?' if shypo[i] != '0' else str(value)
+                ghypo = [g for g in ghypo if g[i] == '?' or shypo[i] == '?']
+        elif row.iloc[-1] == 0 and str(value) != shypo[i] and shypo[i] != '?':
+            temp_list = ['?'] * i + [shypo[i]] + ['?'] * (len(shypo) - i - 1)
+            if temp_list not in ghypo:
+                ghypo.append(temp_list)
 
 print("Final Specific Hypothesis:", shypo)
 print("Final General Hypothesis:", ghypo)
